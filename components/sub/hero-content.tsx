bf5b24a473cb41262ea473b914fa672afc4a9985
Anil Panth <anilpanth44@gmail.com>
2025-01-04 12:55:18 +0530

    changesmade

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -52,7 +52,7 @@ export const HeroContent = () => {
         <motion.a
   variants={slideInFromLeft(1)}
   className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-  href="https://drive.google.com/file/d/1_pg1Uys8Mn-Vp9As1Ys0API8NrjIDKW9/view?usp=sharing"
+  href="https://drive.google.com/file/d/1nTtz3vA7h6CKOtvZU0RvxFp8YhA9Sqb3/view?usp=sharing"
   target="_blank"
   rel="noopener noreferrer"
 >
